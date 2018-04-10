e243ef30ab36230961495045e880c490dc6bb5b5
Binod Mainali <binodmainali1@gmail.com>
2018-04-10 13:50:40 +0530

    Updated seed.rb and faker gem for local development

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,6 +5,8 @@ Types::PostType = GraphQL::ObjectType.define do
   field :id, !types.ID, 'The ID of the blog post'
   field :title, types.String, 'A user-friendly title for the blog post'
   field :body, types.String, 'The main body of content for the blog post'
+  field :user, Types::UserType, 'The user who wrote the blog post'
+  field :comments, types[Types::CommentType], 'Comments that have been posted to the blog post'
   field :short_body, types.String, 'Returns the short description of body' do
     resolve ->(obj, args, ctx) {
     obj.body[0, 5]
